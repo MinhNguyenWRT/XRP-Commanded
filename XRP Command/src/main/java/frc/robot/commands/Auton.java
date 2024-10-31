30f220ae023f0ed01f4545d2cc38362465d4e562
MinhNguyenWRT <minh.nguyen@waltonrobotics.org>
2024-10-31 11:24:39 -0400

    Imported Sensors

--- XRP Command/src/main/java/frc/robot/commands/Auton.java
+++ XRP Command/src/main/java/frc/robot/commands/Auton.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -7,22 +9,26 @@ import frc.robot.subsystems.XRPDrivetrain;
 
 public class Auton extends Command{
   private final XRPDrivetrain m_XRPDrivetrain;
-  private AnalogInput m_leftSensor;
-  private AnalogInput m_rightSensor;
+
+  private final AnalogInput m_leftSensor = new AnalogInput(0);
+  private final AnalogInput m_rightSensor = new AnalogInput(1);
+  private final DoubleSupplier m_leftSensorVol = () -> m_leftSensor.getVoltage();
+  private final DoubleSupplier m_rightSensorVol = () -> m_rightSensor.getVoltage();
+
 
   public Auton(XRPDrivetrain XRPDrivetrain) {
     m_XRPDrivetrain = XRPDrivetrain;
-    m_leftSensor = new AnalogInput(0);
-    m_rightSensor = new AnalogInput(2);
     addRequirements(XRPDrivetrain);
   }
 
     public void initialize() {
       System.out.println("Auton start");
       //Work in Process
+
+      //Auton going in a straight line
       Commands.sequence(
         Commands.race(
-          Commands.run(() -> m_XRPDrivetrain.arcadeDrive(1, 0.5), m_XRPDrivetrain),
+          Commands.run(() -> m_XRPDrivetrain.arcadeDrive(1, 0), m_XRPDrivetrain),
           Commands.waitSeconds(3)
         )
         ).schedule();
